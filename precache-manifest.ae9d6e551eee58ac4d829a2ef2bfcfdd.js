acd146946579ef05db73c9a06248e54c6155e703
WebRazr <razrsvob@gmail.com>
2020-10-02 12:09:48 +0300

    Updates

---
+++ precache-manifest.ae9d6e551eee58ac4d829a2ef2bfcfdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c40d48593038d87c42e64a007c9161d",
+    "url": "/weatherToday/index.html"
+  },
+  {
+    "revision": "69f45872d2408df0d372",
+    "url": "/weatherToday/static/css/main.c0a0c905.chunk.css"
+  },
+  {
+    "revision": "f0a9bbaa36096b5c36f6",
+    "url": "/weatherToday/static/js/2.7615f18f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherToday/static/js/2.7615f18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f45872d2408df0d372",
+    "url": "/weatherToday/static/js/main.b3e82c79.chunk.js"
+  },
+  {
+    "revision": "435c10a5874470935c81",
+    "url": "/weatherToday/static/js/runtime-main.da1b8105.js"
+  }
+]);
